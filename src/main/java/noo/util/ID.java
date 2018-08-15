5148df0c39c821ed891844857cf4bf24fab35874
qu.jianjun <troopson@163.com>
2018-08-15 16:05:55 +0800

    add ID

--- src/main/java/noo/util/ID.java
+++ src/main/java/noo/util/ID.java
@@ -7,7 +7,7 @@ package noo.util;
  * @author qujianjun   troopson@163.com
  * 2018年8月15日 
  */
-public class Guid {
+public class ID {
 
 	public static String uuid() {
 		return java.util.UUID.randomUUID().toString().replace("-", "").toLowerCase();
@@ -17,9 +17,9 @@ public class Guid {
 	public static void main(String[] args) {
 		
 		for(int i=0;i<1000;i++) {
-			System.out.println(Guid.uuid()); 
+			System.out.println(ID.uuid()); 
 		}
-		System.out.println(Guid.uuid().length());
+		System.out.println(ID.uuid().length());
 	}
 	
 }
