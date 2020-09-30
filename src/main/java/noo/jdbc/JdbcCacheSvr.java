3f3df61f714570ecacb5f93fedfafe20cbe75b01
qu.jianjun <troopson@163.com>
2020-09-30 15:53:57 +0800

    add cachesvr

--- src/main/java/noo/jdbc/JdbcCacheSvr.java
+++ src/main/java/noo/jdbc/JdbcCacheSvr.java
@@ -13,6 +13,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import noo.json.JsonArray;
 import noo.json.JsonObject;
 import noo.util.MD5;
+import noo.util.S;
 
 /**
  * @author qujianjun   troopson@163.com
@@ -40,7 +41,7 @@ public class JdbcCacheSvr {
 	}
 
 
-	public JsonArray qry(String sql, JsonObject param, int cache_second) {	
+	public JsonArray qry(String sql, JsonObject param, int second) {	
 		String key = "jdbc.result."+MD5.encode(sql+" param:"+param.encode());
 		
 		if(redis.hasKey(key)) {
@@ -50,11 +51,45 @@ public class JdbcCacheSvr {
 		}else {
 			JsonArray ja = svr.qry(sql, param);
 			if(ja!=null) {
-				redis.opsForValue().set(key, ja.encode(), cache_second, TimeUnit.SECONDS);
+				redis.opsForValue().set(key, ja.encode(), second, TimeUnit.SECONDS);
 			}
 			return ja;
 		}
 		 
+	}
+	
+	
+	public JsonObject get(String table, String pkfield, Object uuid,int second) {
+		String uid = uuid.toString();
+		String key = "jdbc.result."+MD5.encode(table+"."+pkfield+"."+uid);
+		if(redis.hasKey(key)) {
+			log.info("find in cache, return result from cache for table uuid:"+table+"."+uid);
+			String value = redis.opsForValue().get(key);
+			return new JsonObject(value);
+		}else {
+			JsonObject jo = svr.get(table, pkfield, uuid);
+			if(jo!=null) {
+				redis.opsForValue().set(key, jo.encode(), second, TimeUnit.SECONDS);
+			}
+			return jo;
+		}
+		
+	} 
+	
+	public String qryString(String sql, JsonObject param,int second) {
+		String key = "jdbc.result."+MD5.encode(sql+" param:"+param.encode());
+		
+		if(redis.hasKey(key)) {
+			log.info("find in cache, return result from cache for sql:"+sql);
+			String value = redis.opsForValue().get(key);
+			return value;
+		}else {
+			String s = svr.qryString(sql, param);
+			if(S.isNotBlank(s)) {
+				redis.opsForValue().set(key, s, second, TimeUnit.SECONDS);
+			}
+			return s;
+		}
 	}
 	
 }
